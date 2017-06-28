ad281d759dd9bac50a32dc723e88c014b879b431
Jose Centeno <jmcenteno@gmail.com>
2017-06-28 19:53:51 -0400

    set cart item in session when actions are dispatched

--- src/app/actions/cart.js
+++ src/app/actions/cart.js
@@ -1,3 +1,7 @@
+import store from 'store2';
+
+const cart = store.namespace('cart');
+
 export const CART_GET_ITEMS = 'CART_ADD_ITEM';
 export const CART_ADD_ITEM = 'CART_ADD_ITEM';
 export const CART_REMOVE_ITEM = 'CART_REMOVE_ITEM';
@@ -16,18 +20,20 @@ export function getItems() {
 export function addCartItem(product, qty) {
   return function (dispatch, getState) {
 
-    const items = [].concat(...getState().cart.get('items'));
+    const items = getState().cart.get('items').toJS();
 
     items.push({
       product,
       qty
     });
 
+    cart.session('items', items);
+
     dispatch({
       type: CART_ADD_ITEM,
       data: items
     });
-    
+
   };
 }
 
@@ -35,8 +41,10 @@ export function removeCartItem(index) {
   return function (dispatch, getState) {
 
     const items = getState().cart.get('items').toJS();
-    
+
     items.splice(index, 1);
+
+    cart.session('items', items);
 
     dispatch({
       type: CART_REMOVE_ITEM,
