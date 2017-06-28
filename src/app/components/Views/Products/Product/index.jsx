7b5268424a0406915bffdafbe4ef2d0fdbb77c65
Jose Centeno <jmcenteno@gmail.com>
2017-06-28 12:09:33 -0400

    show star ratings

--- src/app/components/Views/Products/Product/index.jsx
+++ src/app/components/Views/Products/Product/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { List } from 'immutable';
 import { Card, CardMedia } from 'material-ui/Card';
 import Radium, { Style } from 'radium';
 
@@ -8,6 +9,9 @@ import StarRatings from '../StarRatings';
 import styles from './styles';
 
 function Product({ product, onClick, ...rest }) {
+
+  const price = Utils.formatCurrency(product.get('price'));
+  const ratings = Utils.calculateRatings((product.get('reviews') || List()).toJS());
 
   const component = (
     <div onClick={ onClick }>
@@ -23,8 +27,8 @@ function Product({ product, onClick, ...rest }) {
           </figure>
           <div className='overlay'>
             <div className='title'>{ product.get('name') }</div>
-            <div className='subtitle'>{ Utils.formatCurrency(product.get('price')) }</div>
-            <StarRatings stars={ product.get('starRatings') || 0 } className='stars' />
+            <div className='subtitle'>{ price }</div>
+            <StarRatings stars={ Number.isNaN(ratings) ? 0 : ratings  } className='stars' />
           </div>
         </CardMedia>
       </Card>
